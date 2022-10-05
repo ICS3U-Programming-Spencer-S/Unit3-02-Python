81fc6246519e3c438fdf07a547da5b1f162648a0
Spencer Scarlett <Spencer.Scarlett@stu.ocsb.ca>
2022-10-05 23:02:18 +0000

    Created num_guess.py and constants.py

---
+++ num_guess.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+ 
+# Created By: Spencer Scarlett
+# Date: Oct. 5, 2022
+# Number guessing program, change constants.py correct_answer to have the correct answer you want
+import constants
+ 
+ 
+def main():
+ 
+    # Spacer
+    print("")
+ 
+    # Obtain user guess for the correct answer
+    print("This is a number guessing game, choose a number between 1-10")
+    guess_answer = int(input("Insert your guess (1 - 10): "))
+ 
+    # Spacer
+    print("")
+ 
+    # Answer checking, seeing if user input is correct
+    if guess_answer == constants.correct_answer:
+        print("Your guess is correct")
+ 
+    else:
+        print("Your answer is incorrect")
+ 
+ 
+if "__main__" == __name__:
+    main()
